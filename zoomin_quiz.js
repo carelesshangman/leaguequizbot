4303ac66804ad531d3fc39b2d8cc8c2681e482b5
Jaka German <anarchyisoffline@gmail.com>
2025-04-05 15:12:30 +0200

    init

---
+++ zoomin_quiz.js
@@ -0,0 +1,34 @@
+const zoomInQuiz = [
+    {
+        answer: ["Darius"],
+        asset: "assets/Darius.jpg",
+        color: "",
+        spoiler: true,
+    },
+    {
+        answer: ["Fiddlesticks", "Surprise party Fiddlesticks"],
+        asset: "assets/surprise_fiddle.jpg",
+        color: "",
+        spoiler: true,
+    },
+    {
+        answer: ["Jhin", "Soul Fighter Jhin"],
+        asset: "assets/soulfighter_jhin.png",
+        color: "",
+        spoiler: true,
+    },
+    {
+        answer: ["Yasuo", "The Ruined King Yasuo", "Yasuo from The Ruined King"],
+        asset: "assets/trkyasuo.png",
+        color: "",
+        spoiler: true,
+    },
+    {
+        answer: ["Briar"],
+        asset: "assets/briar_wallpaper.jpg",
+        color: "",
+        spoiler: true,
+    }
+]
+
+module.exports = zoomInQuiz;
